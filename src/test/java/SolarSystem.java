e31042a4e744bbbfe2743a9d98b0ca60c39d6454
piosso1 <patrick2.udo@gmail.com>
2024-02-17 18:42:00 +0100

    i have work on planet2

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -5,5 +5,10 @@ public class SolarSystem {
         solar.Stars="100";
         solar.Moon="4";
         solar.Planets="2";
+
+        planet2 gecko =new planet2();
+        gecko.color="magenta";
+        gecko.temp="34343443434k";
+        gecko.size="342344342m";
     }
 }
